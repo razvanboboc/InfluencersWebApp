2f860179f4f0f57c76d0e53ca5d25a6eb39787bf
razvanboboc <rzvn11@gmail.com>
2020-06-24 12:49:39 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Influencers.Models;
+
+namespace Influencers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddArticle()
+        {
+            ViewBag.Current = "Scheduler";
+            return View();
+        }
+
+        public IActionResult JoinInfluencers()
+        {
+            ViewBag.Current = "Scheduler";
+            return View();
+        }
+
+        public IActionResult Ranking()
+        {
+            ViewBag.Current = "Scheduler";
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
