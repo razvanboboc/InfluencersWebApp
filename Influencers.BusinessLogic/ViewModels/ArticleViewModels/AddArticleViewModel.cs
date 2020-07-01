f931ce006faf0407ade0f058baf07d8070b07f8f
razvanboboc <rzvn11@gmail.com>
2020-07-01 20:27:35 +0300

    added field validations for add author and add article

--- Influencers.BusinessLogic/ViewModels/ArticleViewModels/AddArticleViewModel.cs
+++ Influencers.BusinessLogic/ViewModels/ArticleViewModels/AddArticleViewModel.cs
@@ -8,7 +8,7 @@ namespace Influencers.BusinessLogic.ViewModels.ArticleViewModels
 {
     public class AddArticleViewModel
     {
-        [Required(ErrorMessage ="")]
+        [Required(ErrorMessage ="Please input a valid email")]
         [StringLength(100)]
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")]
         public string Email { get; set; }
--- Influencers.BusinessLogic/ViewModels/AuthorViewModels/AddAuthorViewModel.cs
+++ Influencers.BusinessLogic/ViewModels/AuthorViewModels/AddAuthorViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Influencers.BusinessLogic.ViewModels.AuthorViewModels
 {
     public class AddAuthorViewModel
     {
+        [Required]
         public string Nickname { get; set; }
+
+        [Required]
         public string Email { get; set; }
     }
 }
